741cc6447182025352753afd22a3e90ca3f96f30
RuiTakao <ruia1082halfnc@gmail.com>
2023-04-20 06:58:30 +0900

    命令追加

--- src/ts/strage/Demand.ts
+++ src/ts/strage/Demand.ts
@@ -1,27 +1,37 @@
+import { truthDare } from "./truthDare";
+
 class Demand {
   private demand: string = "";
-  private demandList: string[] = [
-    "今日の出来事",
-    "黒歴史",
-    "昨日した失敗",
-    "3サイズ",
-    "恥ずかしいこという",
-    "なんで昨日遅刻した？",
-  ];
 
   get getDemand(): string {
     return this.demand;
   }
 
   get getDemandList(): string[] {
-    return this.demandList;
+    switch (truthDare.getChooseContent) {
+      case "truth":
+        return [
+          "黒歴史",
+          "昨日した失敗",
+          "お金ない理由",
+          "直近の恥ずかしい体験",
+          "3サイズ",
+        ];
+      case "dare":
+        return [
+          "モノマネする",
+          "変顔する",
+          "10秒以内にオリジナルの歌を歌う",
+          "1分間、瞬き禁止",
+          "30秒以内に山手線の駅名全部書く",
+        ];
+      default: return [];
+    }
   }
 
   setDemand = (demand: string): void => {
     this.demand = demand;
   };
-
-  setDemandList() {}
 }
 
 export const demand: Demand = new Demand();
